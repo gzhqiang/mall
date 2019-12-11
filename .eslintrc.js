0e0e2e3f1eeb519d907e8a4531a1c1075304b458
zqguo <gzhqiang123@gmail.com>
2019-12-11 11:31:27 +0800

    modify config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,13 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': ['error', 'never'],
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+      vue: 'never'
+    }],
+    'object-curly-spacing': ['error', 'never'],
+    'arrow-parens': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
